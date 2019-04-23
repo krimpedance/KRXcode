32dca3dbdacb98f2e50b40b5604eb0de8851f3a6
Krimpedance <info@krimpedance.com>
2019-04-23 18:27:12 +0900

    Add version command

--- lib/krxcode/cli.rb
+++ lib/krxcode/cli.rb
@@ -4,6 +4,11 @@ require 'thor'
 module Krxcode
   class CLI < Thor
 
+    desc "version", "KRXcode version."
+    def version
+      puts Krxcode::VERSION
+    end
+
     desc "hello_world", "hello world test"
     def hello_world
       puts 'hello ;)'
