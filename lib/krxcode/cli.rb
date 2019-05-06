bda7ebf9e11b7324804d153b4314e10f741fecf0
Krimpedance <info@krimpedance.com>
2019-05-06 14:11:31 +0900

    Add sort command

--- lib/krxcode/cli.rb
+++ lib/krxcode/cli.rb
@@ -1,6 +1,7 @@
 require 'krxcode'
 require 'thor'
 require 'krxcode/generator'
+require 'xcodeproj'
 
 module Krxcode
   class CLI < Thor
@@ -9,6 +10,13 @@ module Krxcode
     desc "version", "KRXcode version."
     def version
       puts Krxcode::VERSION
+    end
+
+    desc "sort [MyProject.xcodeproj]", "Sort project files"
+    def sort file
+      project = Xcodeproj::Project.open(file)
+      project.sort(groups_position: :above)
+      project.save
     end
 
     register(Generator, "generate", "generate [COMMAND]", "generate commands.", alias: "g")
